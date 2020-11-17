d2ff7fec9bbba1d5adf4cb6dcca1303499dc7c85
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-11-17 13:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solutions in this file
-const t=["Lisa","Kaitlin", "Jan"];
+const t=['Lisa','Kaitlin', 'Jan'];
 const s='surprise';
 
 function writeCards(w , z){
-  let a=["Thank you,"+w[0]+ ",for the wonderful " +z+" gift!"];
+  let a=['Thank you,'+w[0]+' for the wonderful ' +z+' gift!'];
   for (let i=1; i<w.length;i++){
    
-   a.push("Thank you,"+w[i]+ ",for the wonderful " +z+" gift!");
+   a.push('Thank you,'+w[i]+ ',for the wonderful ' +z+' gift!');
 
   }
   return a;
